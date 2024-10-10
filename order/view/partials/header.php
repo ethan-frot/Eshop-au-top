bc084d86ea24f723b6d09ec0ca571fcd79ca754f
Ethan <github.xdgcd@passmail.net>
2024-10-10 13:22:00 +0200

    🎉Init project

---
+++ order/view/partials/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Eshop au top</title>
+	</head>
+	<body>
+
+	<header>
+		<h1>Le Eshop au top</h1>
+
+		<nav>
+			<ul>
+				<li><a href="http://localhost:8888/esd-oop-php/">Commander</a></li>
+				<li><a href="http://localhost:8888/esd-oop-php/set-shipping-address">Renseigner l'adresse de livraion</a></li>
+				<li><a href="http://localhost:8888/esd-oop-php/set-shipping-method">Renseigner la méthode de livraison</a></li>
+				<li><a href="http://localhost:8888/esd-oop-php/pay">Payer</a></li>
+
+
+			</ul>
+		</nav>
+	</header>
