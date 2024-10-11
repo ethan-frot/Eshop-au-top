d2540bd6a85546fd3cb2250004a317418f95f549
Ethan <github.xdgcd@passmail.net>
2024-10-11 13:11:23 +0200

    🚸Better redirection on error

--- product/view/product-created.php
+++ product/view/product-created.php
@@ -3,7 +3,7 @@
 <main class="d-flex justify-content-center align-items-center flex-column text-center">
     <h1 class="display-4">Produit créé</h1>
     <p class="lead">Le produit a été créé avec succès.</p>
-    <a href="http://localhost:8888/Eshop-au-top/list-products" class="btn btn-primary">Retour à la liste des produits</a>
+    <a href="http://localhost:8888/Eshop-au-top/list-products" class="btn btn-primary">Liste des produits</a>
 </main>
 
 <?php require_once('./order/view/partials/footer.php'); ?>
